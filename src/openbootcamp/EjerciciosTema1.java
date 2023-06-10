7108e1f9d2feeb1d5076e81c1d84cc77b0003176
“Claudia <claudia.ortiz@uc.cl>
2023-06-09 22:00:07 -0400

    Ejercicio Tema 1 OpenBootcamp

---
+++ src/openbootcamp/EjerciciosTema1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package openbootcamp;
+
+/**
+ *
+ * @author clau
+ */
+public class EjerciciosTema1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+
+        // Tipos de datos
+        byte numeroByte = 10;
+        short numeroShort = 1000;
+        int numeroInt = 100000;
+        float numeroFloat = 3.1416f;
+        double numeroDouble = 3.14159265359;
+        boolean esVerdadero = true;
+        char caracter = 'A';
+        String texto = "Hola, mundo!";
+
+        // Imprimir los datos
+        System.out.println("Número byte: " + numeroByte);
+        System.out.println("Número short: " + numeroShort);
+        System.out.println("Número int: " + numeroInt);
+        System.out.println("Número float: " + numeroFloat);
+        System.out.println("Número double: " + numeroDouble);
+        System.out.println("¿Es verdadero?: " + esVerdadero);
+        System.out.println("Carácter: " + caracter);
+        System.out.println("Texto: " + texto);
+    }
+}
+
+    
+    
+
